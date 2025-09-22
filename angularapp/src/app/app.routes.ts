206dee305ae4d5d093df91ae8b43d1e247775534
Beka <Bekarys.Alashybayev@kaspi.kz>
2025-09-22 12:55:41 +0500

    feat: update routing paths for Deposit and Gold pages to include bank prefix

--- angularapp/src/app/app.routes.ts
+++ angularapp/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { CreditComponent } from './pages/credit/credit';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'bank/credit' },
-];
   { path: 'bank/credit', component: CreditComponent },
   { path: '**', redirectTo: 'bank/credit' }
+];
+
